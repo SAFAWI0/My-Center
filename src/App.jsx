6a0e19009fb7dac7be7c5507396f35c6329cdfb7
SAFAWI0 <safaa.ahmad.2000.9@gmail.com>
2024-01-14 16:33:19 +0300

    update

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import { Route, Routes } from "react-router-dom";
 import LoginScreen from "./screens/LoginScreen/loginScreen";
 import RegisterScreen from "./screens/RegisterScreen/registerScreen";
 import { HomeScreen } from "./screens/HomeScreen/homeScreen";
-import { Page } from "./app/page/page";
+
 
 
 function App() {
@@ -11,7 +11,7 @@ function App() {
     <div>
       <Routes>
         <Route path="/" element={<HomeScreen />} />
-        <Route path=":id" element={<Page />} />
+  
         <Route path="/login" element={<LoginScreen />} />
         <Route path="/Register" element={<RegisterScreen />} />
       </Routes>
