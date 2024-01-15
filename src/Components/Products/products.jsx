7a52bd63e904ca611764cc32928c055ef76561c0
SAFAWI0 <safaa.ahmad.2000.9@gmail.com>
2024-01-16 01:15:48 +0300

    update

--- src/Components/Products/products.jsx
+++ src/Components/Products/products.jsx
@@ -4,6 +4,8 @@ import { SearchOutlined } from "@ant-design/icons";
 import Container from "../Container/container";
 import BannerSlider from "../Banner/banner";
 import { Card } from "../Card/card";
+import { Categories } from "../Categories/categories";
+import { GridItems } from "../GridItems/gridItems";
 export const Products = () => {
   return (
     <div>
@@ -22,6 +24,8 @@ export const Products = () => {
           />
         </div>
         <BannerSlider />
+        <Categories />
+        <GridItems />
         <Card />
       </Container>
     </div>
