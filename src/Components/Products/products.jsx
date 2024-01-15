1866cff1f2aeed42ad2e9fce1e672a1271fabe19
SAFAWI0 <safaa.ahmad.2000.9@gmail.com>
2024-01-16 01:17:30 +0300

    update

--- src/Components/Products/products.jsx
+++ src/Components/Products/products.jsx
@@ -5,7 +5,6 @@ import Container from "../Container/container";
 import BannerSlider from "../Banner/banner";
 import { Card } from "../Card/card";
 import { Categories } from "../Categories/categories";
-import { GridItems } from "../GridItems/gridItems";
 export const Products = () => {
   return (
     <div>
@@ -25,7 +24,6 @@ export const Products = () => {
         </div>
         <BannerSlider />
         <Categories />
-        <GridItems />
         <Card />
       </Container>
     </div>
