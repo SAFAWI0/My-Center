a5bf3c42f7a3b95ae4ff09acc75b47aff819ee6d
SAFAWI0 <safaa.ahmad.2000.9@gmail.com>
2024-01-15 19:23:29 +0300

    update

---
+++ src/page/page.jsx
@@ -0,0 +1,33 @@
+import "./page.css";
+import { useParams } from "react-router-dom";
+import { FaStar } from "react-icons/fa";
+import Container from "../Components/Container/container";
+
+
+export const Page = () => {
+  const { id } = useParams();
+
+  return (
+    <div>
+      <Container>
+        <div className="page-content">
+          <div className="image">
+            <img className="background" src="https://picsum.photos/900/800" />
+            <div className="content">
+              <h1>Beauty</h1>
+              <img className="logo" src="https://picsum.photos/900/900" />
+            </div>
+          </div>
+          <div className="evaluation">
+            <FaStar style={{ color: "gold" }} />
+            <p>
+              2.5
+              : تقييم المركز</p>
+          </div>
+            <span>center is good</span>
+          <p>:ايام الدوام</p>
+        </div>
+      </Container>
+    </div>
+  );
+};
