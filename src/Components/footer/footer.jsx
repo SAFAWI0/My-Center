ea0409dd285ce4008af55c90ad9a6213fe09fcce
SAFAWI0 <safaa.ahmad.2000.9@gmail.com>
2024-01-17 13:13:08 +0300

    update

--- src/Components/footer/footer.jsx
+++ src/Components/footer/footer.jsx
@@ -2,8 +2,8 @@ import "./footer.css";
 import Container from "../Container/container";
 import imag1 from "../../assets/facebook.png";
 import imag2 from "../../assets/instagram.png";
-import imag3 from "../../assets/aon.png";
 import { FaXTwitter } from "react-icons/fa6";
+
 export const Footer = () => {
   return (
     <div className="footer">
@@ -11,17 +11,14 @@ export const Footer = () => {
         <div className="content">
           <div className="title">
             <p>By - Aon 2023 </p>
-            <img src={imag3} alt="imageIcon" />
           </div>
           <ul>
             <li>
               <img src={imag1} alt="imageIcon" />
             </li>
-
             <li>
               <img src={imag2} alt="imageIcon" />
             </li>
-
             <li>
               <FaXTwitter />
             </li>
