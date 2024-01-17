bf95b227183e4b66c0986353cdb7460e4af8de9d
jasem mo <126420655+J10MO@users.noreply.github.com>
2024-01-17 21:13:34 +0300

    1

--- src/screens/HomeScreen/homeScreen.jsx
+++ src/screens/HomeScreen/homeScreen.jsx
@@ -4,11 +4,6 @@ import { Products } from "../../Components/Products/products";
 import { ViewBox } from "../../Components/ViewBox/viewBox";
 import { Footer } from "../../Components/footer/footer";
 
-
-<<<<<<< HEAD
-=======
-
->>>>>>> c5e534f30c44e6374a2bc8836f53a0a419734f17
 export const HomeScreen = () => {
   return (
     <div>  
