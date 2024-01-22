f4b32753e60acfc059433de06736b527812664fc
SAFAWI0 <safaa.ahmad.2000.9@gmail.com>
2024-01-21 18:08:23 -0800

    update to com..viewBox

--- src/Components/Sessioncard/sessioncard.jsx
+++ src/Components/Sessioncard/sessioncard.jsx
@@ -18,14 +18,11 @@ import Container from "../Container/container";
 export const Sessioncard = () => {
   return (
     <div>
-
       <Container>
         <div className="contentt">
           <div className="coverblock">
-            {" "}
             <img src={covere} className="cover"></img>
           </div>
-
           <div className="like">
             <LikeButton />
           </div>
@@ -40,29 +37,31 @@ export const Sessioncard = () => {
             <div className="right">
               <h2>السعر</h2>
             </div>
-
-    <Container>
-    <div className="content">
-    <Link to={HomeScreen}><div className="coverblock"> <img src={covere} className="cover"></img></div></Link>
-     
-      <div className="like">
-        <LikeButton />
-      </div>
-      <div className="foot">
-        <div className="left">
-          <h2>اسم الجلسة المباركه</h2>
-          <div className="reting">
-          <CiStar className="star" /><span>2.5</span>
-
           </div>
         </div>
-        <div className="right">
-          <h2>السعر</h2>
+        <div className="content">
+          <Link to={HomeScreen}>
+            <div className="coverblock">
+              <img src={covere} className="cover"></img>
+            </div>
+          </Link>
+          <div className="like">
+            <LikeButton />
+          </div>
+          <div className="foot">
+            <div className="left">
+              <h2>اسم الجلسة المباركه</h2>
+              <div className="reting">
+                <CiStar className="star" />
+                <span>2.5</span>
+              </div>
+            </div>
+            <div className="right">
+              <h2>السعر</h2>
+            </div>
+          </div>
         </div>
-      </div>
+      </Container>
     </div>
-  </Container>
-  
-</div>
   );
 };
